4d13ed3ef6e396c9b9d796019f27763da908680a
Ed Rakhmankulov <erixonich@gmail.com>
2022-04-27 19:23:40 +0300

    added naive multithreading for task

--- application/src/main/java/com/gridgain/ignite/ggnode/cgrid/SumBalancesForAllClientsTask.java
+++ application/src/main/java/com/gridgain/ignite/ggnode/cgrid/SumBalancesForAllClientsTask.java
@@ -3,19 +3,31 @@ package com.gridgain.ignite.ggnode.cgrid;
 import com.gridgain.ignite.ggnode.model.entities.Account;
 import com.gridgain.ignite.ggnode.model.entities.AccountKey;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Spliterator;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import javax.cache.Cache;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.binary.BinaryObject;
+import org.apache.ignite.cache.CachePeekMode;
+import org.apache.ignite.cache.query.QueryCursor;
+import org.apache.ignite.cache.query.ScanQuery;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.compute.ComputeJob;
 import org.apache.ignite.compute.ComputeJobAdapter;
 import org.apache.ignite.compute.ComputeJobResult;
 import org.apache.ignite.compute.ComputeTaskAdapter;
+import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.processors.cache.distributed.dht.topology.GridDhtLocalPartition;
+import org.apache.ignite.internal.util.typedef.internal.CU;
 import org.apache.ignite.resources.IgniteInstanceResource;
 
 import static java.util.function.Function.identity;
@@ -54,24 +66,44 @@ public class SumBalancesForAllClientsTask extends ComputeTaskAdapter<BigDecimal,
 
         @Override public Object execute() throws IgniteException {
             IgniteCache<BinaryObject, BinaryObject> cache = ignite.cache(Account.CACHE_NAME).withKeepBinary();
-            Map<Long, BigDecimal> res = new HashMap<>();
+            ConcurrentHashMap<Long, BigDecimal> res = new ConcurrentHashMap<>();
 
-            for (Cache.Entry<BinaryObject, BinaryObject> e : cache.localEntries()) {
-                Long clientId = e.getKey().field("clientId");
-                BigDecimal balance = e.getValue().field("balance");
-                res.compute(
-                    clientId,
-                    (k, v) -> {
-                        if (v == null)
-                            return balance;
-                        else
-                            return v.add(balance);
-                    }
+            int[] parts = ignite.affinity(Account.CACHE_NAME).primaryPartitions(ignite.cluster().localNode());
+            List<Future<?>> futs = new ArrayList<>();
+            for (int part : parts) {
+                QueryCursor<Cache.Entry<BinaryObject, BinaryObject>> cursor
+                    = cache.query(new ScanQuery<BinaryObject, BinaryObject>(part).setLocal(true));
+
+                futs.add(
+                    ForkJoinPool.commonPool().submit(
+                        () -> {
+                            for (Cache.Entry<BinaryObject, BinaryObject> e : cursor) {
+                                Long clientId = e.getKey().field("clientId");
+                                BigDecimal balance = e.getValue().field("balance");
+
+                                res.compute(
+                                    clientId,
+                                    (k, v) -> {
+                                        if (v == null) return balance;
+                                        else return v.add(balance);
+                                    }
+                                );
+                            }
+                        }
+                    )
                 );
             }
 
+            for (Future<?> fut : futs) {
+                try {
+                    fut.get();
+                } catch (Exception e) {
+                    throw new IgniteException(e);
+                }
+            }
+
             if (threshold != null) {
-                res = res.entrySet().stream()
+                return res.entrySet().stream()
                     .filter(entry -> threshold.compareTo(entry.getValue()) > 0)
                     .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
             }
